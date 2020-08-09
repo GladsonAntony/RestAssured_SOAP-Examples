f1c156398e3b8e2875e8b6f0ef0ce8edf2698e70
Gladson Antony <GladsonAntony91@gmail.com>
2020-08-09 17:51:50 +0530

    Updated Excel Data Providers

--- src/main/java/controllers/ExcelDataProvider.java
+++ src/main/java/controllers/ExcelDataProvider.java
@@ -1,16 +1,14 @@
 package controllers;
 
+import org.testng.annotations.DataProvider;
+import utils.ExcelUtils;
+
 import java.io.File;
 import java.lang.reflect.Method;
-
-import org.testng.annotations.DataProvider;
-
-import utils.ExcelUtils;
 
 /**
  * @Author Gladson Antony
  * @Date 2020-08-09T17:33:16+05:30
- *
  */
 
 public class ExcelDataProvider extends InitMethod {
@@ -19,15 +17,13 @@ public class ExcelDataProvider extends InitMethod {
         File file = new File("src/test/resources/Excel Files/TestData.xlsx");
         String SheetName = method.getName();
         System.out.println(SheetName);
-        Object testObjArray[][] = ExcelUtils.getTableArray(file.getAbsolutePath(), SheetName);
-        return testObjArray;
+        return ExcelUtils.getTableArray(file.getAbsolutePath(), SheetName);
     }
 
     @DataProvider(name = "excelSheetNameAsMethodName", parallel = true)
     public static Object[][] excelSheetNameAsMethodName(Method method) throws Exception {
         File file = new File("src/test/resources/Excel Files/" + method.getName() + ".xlsx");
         System.out.println("Opening Excel File:" + file.getAbsolutePath());
-        Object testObjArray[][] = ExcelUtils.getTableArray(file.getAbsolutePath());
-        return testObjArray;
+        return ExcelUtils.getTableArray(file.getAbsolutePath());
     }
 }
